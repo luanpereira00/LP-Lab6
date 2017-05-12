328537d6fbbe2efba1b531c01a94885db0d7b78d
Luan <luanpereirast@gmail.com>
2017-05-12 08:21:42 -0300

    Adicionando inicializações de implementação de lista ligada

--- test/lista.cpp
+++ test/lista.cpp
@@ -1,5 +1,27 @@
 #include "lista.h"
 
-int main (){
-	return 0;
+lista::lista(){
+	//faz nada
 }
+
+void lista::criar(){
+	this->inicio=0;
+}
+
+void lista::inserir(int el){
+	struct node **it = (node**)&(this->inicio);
+	struct node *tmp = new node;
+	tmp->dado=el;
+	tmp->prox= *it;
+	*it = tmp;
+}
+
+struct node** lista::busca(int pos){
+	struct node **it = (node**)&(this->inicio);
+	for (int i=0; i<pos; i++){
+		if((*it)->prox) return 0;
+		it = &((*it)->prox);
+	}
+	return it;
+}
+
--- test/lista.h
+++ test/lista.h
@@ -2,13 +2,13 @@
 #define LISTA_H
 
 //template <typename T>
-struct  node{
+struct node{
 	int dado;
-	node* prox;
+	struct node* prox;
 	//node* anter;
 };
 struct head{
-	node* inicio;
+	struct node* inicio;
 	//node* fim;
 };
 /*struct tail{
@@ -18,33 +18,17 @@ struct head{
 
 //template <typename T>
 class lista{
-	head* inicio;
-	node* vazio;
 private:
-	head* start = new head[1];
+	struct head* inicio;
+
+/*	head* start;*/
 	//tail* end = new tail[1];
 
 public:
-	void criar(head *ll){
-		this->vazio=0;
-		ll->inicio = this->vazio;	
-	}
-	void setElement(head *ll, int el){
-		node** it = &(ll->inicio);
-		node* tmp = new node[1];
-		tmp->dado=el;
-		tmp->prox= *it;
-		*it=tmp;
-	}
-	node** busca(head *ll, int pos){
-		node** it = &(ll->inicio);
-		for(int i=0; i<pos; i++){
-			if(it[0].prox==this->vazio) return &this->vazio;
-			it=&((*it)->prox);
-		}
-		return it;
-	}
-	
+	lista();
+	void criar();
+	void inserir(int el);
+	struct node** busca(int el);
 };
 
 #endif
